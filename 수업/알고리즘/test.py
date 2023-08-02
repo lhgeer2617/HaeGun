b03b27ae9758feada724925d775c93f66ef686c1
HaeGun <lhgeer2617@gmail.com>
2023-08-02 23:36:51 +0900

    d

--- 수업/알고리즘/test.py
+++ 수업/알고리즘/test.py
@@ -1,23 +1,23 @@
-# T = int(input())
+T = int(input())
 
-# for text_case in range(T):
-#     N = int(input())
+for text_case in range(T):
+    N = int(input())
+    result = []
 
-#     bus_routes = [(list(map(int, input().split()))) for _ in range(N)]
+    bus_routes = [(list(map(int, input().split()))) for _ in range(N)]
     
-#     P = int(input())
-#     counts = [0] * (P)
+    P = int(input())
+    counts = [0] * P
 
-#     bus_stations = [int(input()) for _ in range(P)]
+    bus_stations = [int(input()) for _ in range(P)]
 
-#     for a in range(N):
-#         for point in range(bus_routes[a][0]-1, bus_routes[a][1]):
-#             counts[point] += 1
+    for a in range(N):
+        for point in range(bus_routes[a][0]-1, bus_routes[a][1]):
+            counts[point] += 1
 
-#     result = [(counts[bus_stations[b]-1]) for b in range(P)]
+    
 
-#     print(f'#{text_case + 1}', *result)
+    for x in bus_stations:
+        result.append(counts[x-1])
 
-
-########################################################
-
+    print(f'#{text_case + 1}', *result)
