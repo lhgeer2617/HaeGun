3d86460282360fe941686b0b15c551b34183e43a
HaeGun <lhgeer2617@gmail.com>
2023-08-03 17:46:01 +0900

    d

---
+++ 수업/알고리즘/선택정렬.py
@@ -0,0 +1,15 @@
+data = [64, 25, 10, 22, 11]
+
+def my_sort(data, N):
+
+    for i in range(N-1):
+        min_index = i
+        for j in range(i, N):
+            if data[min_index] > data[j]:
+                min_index = j
+        data[i], data[min_index] = data[min_index], data[i]
+
+    print(data)
+
+
+my_sort(data, len(data))
