7d067ca52ac9c214b81dca752cc7f0f30d61f76a
HaeGun <lhgeer2617@gmail.com>
2023-08-10 21:02:55 +0900

    d

--- 수업/알고리즘/test.py
+++ 수업/알고리즘/test.py
@@ -1,49 +1,15 @@
-class stack:
-    def __init__(self):  # 스택 객체 생성
-        self.items = []
-    def push(self, item):  # 스택 요소 추가 push(.append())
-        self.items.append(item)
-    def pop(self):  # 스택 맨 뒤 요소 삭제하고 리턴 pop()
-        try:
-            self.items.pop()
-        except IndexError:
-            exit(0)
-    def top(self):  # 스택 맨 뒤 요소 리턴
-        try:
-            return self.items[-1]
-        except IndexError:
-            exit(0)
-    def isEmpty(self):  # 스택이 비었는지 확인(비었으면 True 리턴)
-        return not self.items
-    def len_stack(self): # 스택 내의 데이터 개수를 확dls
-        return len(self.items)
-
-s = stack()
-t = int(input())
-for tc in range(1, t+1):
-    text = input()
-    size = len(text)
-    stack = [0]*size
-    for i in text:
-        if i == '{' or i == '[' or i == '(':
-            s.push(i)
-        elif i == ')':
-            if s.top() == '(':
-                s.pop()
-            else:
-                break
-        elif i == '}':
-            if s.top() == '{':
-                s.pop()
-            else:
-                break
-        elif i == ']':
-            if s.top() == '[':
-                s.pop()
-            else:
-                break
-    if s.len_stack() == 0:
-        result = 1
-    else :
-        result = 0
-    print(f'#{tc} {result}')
+T = int(input())
+for tc in range(1, T+1):
+    stack = []             # 쇠막대기를 스택에 넣어 보관하고 레이저가 보관된만큼 자를 것이다.
+    input_str = input()
+    cnt = 0                # 레이저가 쇠막대기를 자르는 수를 셀것이다(그만큼 쇠막대기 수가 증가)
+    num = 0                # 쇠막대기가 몇개인지 세어줄것이다.
+    for i in range(len(input_str)):
+        if input_str[i] == '(' and input_str[i + 1] == ')':  # () 이 둘이 붙어 있으면 레이저이다.
+            cnt += len(stack)                                # 레이저는 막대기 갯수만큼 자를 것이다.
+        elif input_str[i] == '(':                            # ( 가 혼자있다면 막대기의 시작점이다. 그만큼 스택에 추가
+            stack.append(input_str[i])
+            num += 1                                         # append 될때마다 num을 증가시켜 쇠막대기 갯수를 구한다.
+        elif input_str[i] == ')' and input_str[i - 1] != '(':  # )가 혼자 있다면 막대기의 끝지점이가. 막대기를 스택에서 하나 빼준다.
+            stack.pop()
+    print(f'#{tc} {cnt + num}')                              # 총 막대기 갯수와 잘라내서 생겨난 수를 더해준다.
