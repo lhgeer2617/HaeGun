5575ee2cc88ee1250ef7501e0f4aca1a9e62fbaf
= <=>
2023-09-21 17:48:30 +0900

    d

--- 수업/알고리즘/9-19/test.py
+++ 수업/알고리즘/9-19/test.py
@@ -1,33 +1,26 @@
-def dijkstra(start):
+def fnc(start):
     distance = [[0xffffff] * N for _ in range(N)]
-    visited = set(start)
-    now_i, now_j = start
+    visited = [[-1] * N for _ in range(N)]
 
-    distance[now_i][now_j] = 0
-    while len(visited) < V:
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
 
-        for next in range(V):
-            if next not in visited and graph[now][next] != -1:
-                if distance[next] > graph[now][next] + distance[now]:
-                    distance[next] = graph[now][next] + distance[now]
+    si, sj = start
 
-        min_dis = 0xffffff
-        min_node = -1
-        for node in range(V):
-            if node not in visited and min_dis > distance[node]:
-                min_dis = distance[node]
-                min_node = node
-
-        now = min_node
-        visited.append(now)
-
-    return distance
+    for k in range(4):
+        ni = si + di[k]
+        nj = sj + dj[k]
 
 
-T = int(input())
+
+
+
+
+T = int(input().split())
+
 for tc in range(1, T + 1):
     N = int(input())
 
     data = [list(map(int, input().split())) for _ in range(N)]
 
-    dijkstra((0,0))
+    fnc((0, 0))
